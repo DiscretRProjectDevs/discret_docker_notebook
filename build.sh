af8ba61c7e624ae307c1c5b2c56bde98b69b8df8
Raph <orba6563@s-kimo.home>
2021-04-21 14:52:55 +0200

    add doc to explain what to do with the image

--- build.sh
+++ build.sh
@@ -8,3 +8,19 @@ HERE=$( dirname "$0" )
 docker build \
        -t "${HPC_REGISTRY_NAME}/${IMAGE_NAME}" \
        "${HERE}"
+
+status=$?
+
+if [[ $status -eq 0 ]]
+then
+    echo "Build OK.
+To upload the image to the registry, please type
+
+	docker image push ${HPC_REGISTRY_NAME}/${IMAGE_NAME}
+
+after having logged in to the registry using
+	docker login ${HPC_REGISTRY_NAME}
+with the credentials you use to access the s-hpc infrastructure
+" 1>&2
+fi
+
